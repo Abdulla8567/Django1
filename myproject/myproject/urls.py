22737e47318cb9d97979063d611be775df1a09fa
abdulla <abdullashaik8567@gmail.com>
2025-12-09 16:35:56 +0530

    default home

--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -20,6 +20,7 @@ from basic.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('',home, name='home'),
     path("home/",home,name="home"),
     path("about/",about,name="about")
 ]
